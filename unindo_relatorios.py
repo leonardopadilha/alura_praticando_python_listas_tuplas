43e95b998bdd7e54a3073206d8950b520b7d91de
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-09-05 07:06:14 -0300

    Unindo relatórios

---
+++ unindo_relatorios.py
@@ -0,0 +1,5 @@
+produto1 = tuple(input("Produtos do estoque 1 (separados por vírgula): ").split(", "))
+produto2 = tuple(input("Produtos do estoque 2 (separados por vírgula): ").split(", "))
+
+produtos = produto1 + produto2
+print(produtos)
